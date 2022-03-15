4866ab055c278a116722d9b84b96857b463a71ba
LevshaAnna <levsha_anna@mail.ru>
2022-03-15 13:42:25 +0300

    .

--- js/FilterComponent.js
+++ js/FilterComponent.js
@@ -1,10 +1,8 @@
 Vue.component('filter-cart', {
-    data {
-    userSearch: ' ',
-},
+
     template: `
-            <form action="#" class="search-form" @submit.prevent="$parent.products.filter(userSearch)">
-            <input type="text" class="search-field" v-model="userSearch">
+            <form action="#" class="search-form" @submit.prevent='$parent.filter'>
+            <input type="text" class="search-field" v-model='$parent.userSearch'>
             <button class="btn-search" type="submit">
                 <i class="fas fa-search"></i>
             </button>
